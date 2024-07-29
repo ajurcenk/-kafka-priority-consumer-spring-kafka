a14e270bc9e38cef5981bbf69774c3b471bf7a6f
ajurcenk <aleksej.jurcenko@redpanda.com>
2024-07-29 18:24:50 -0400

    chnaged application shutdown logic

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Scope;
 import org.springframework.context.event.EventListener;
@@ -140,28 +141,36 @@ public class DemoApplication {
 	public static void main(String[] args) {
 
 
-		addSignalHandler();
 
 		// addShutdownHook will catch System.exit command of the signal handler.
 		// If you have your own shutdown process, you can add it to this shutdown hook.
-		Runtime.getRuntime().addShutdownHook(
-				new Thread(() -> System.out.println("Gracefully shoutdown process is started!")));
+//		Runtime.getRuntime().addShutdownHook(
+//				new Thread(() -> System.out.println("Gracefully shoutdown process is started!")));
 
-		SpringApplication.run(DemoApplication.class, args).close();
+		ConfigurableApplicationContext appCtx = SpringApplication.run(DemoApplication.class, args);
+
+		addSignalHandler(appCtx);
 	}
 
-	private static void addSignalHandler() {
-		SignalHandler signalHandler = new SignalHandlerImpl();
+	private static void addSignalHandler(ConfigurableApplicationContext appCtx) {
+		SignalHandler signalHandler = new SignalHandlerImpl(appCtx);
 		Signal.handle(new Signal("TERM"), signalHandler);
 		Signal.handle(new Signal("INT"), signalHandler);
 	}
 
 	private static class SignalHandlerImpl implements SignalHandler {
 
+		private ConfigurableApplicationContext appCtx = null;
+
+		public SignalHandlerImpl(ConfigurableApplicationContext appCtx) {
+
+			this.appCtx = appCtx;
+		}
 		@Override
 		public void handle(Signal signal) {
 			DemoApplication.isRunning = false;
 			System.out.println("Handling signal" + signal.getName());
+			appCtx.close();
 		}
 	}
 
